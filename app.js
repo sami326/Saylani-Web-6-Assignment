8e44a3b11e9b6b4ff73090b266738918ed9814a5
Abdul Raffay Rizwan <zaimeali1997@gmail.com>
2020-06-21 17:56:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// Chapter 21 - 25
+        // Question 1
+                //   let fname = prompt("Enter your First Name: ");
+                //   let lname = prompt("Enter your Last Name: ");
+                //   let fullName = fname + " " + lname;
+                //   document.write("<h1>Hello, " + fullName + "!</h1>");
+
+        // Question 2
+                //    let phone = prompt("What is your Favorite Mobile Phone Model?");
+                //    let phNum = phone.length;
+                //    document.write("<h1>My Favorite Mobile Model is, " + phone + "!</h1>");
+                //    document.write("<h2>The Length Of Phone Model is, " + phNum + "!</h2>");
+
+        // Question 3
+                //       let str = "Pakistan";
+                //       document.write("<h1>String is = " + str + "</h1>");
+                //       let numIndex = str.indexOf("n");
+                //       document.write("<h2>The index Of n is, " + numIndex + "</h2>");
+
+        // Question 4
+                //       let str = "Hello World";
+                //       document.write("<h1>String = " + str + "</h1>");
+                //       let n = str.lastIndexOf("l");
+                //       document.write("<h2>Last index of 'l' = " + n + "</h2>");
+
+
